4256f622e03bcc21f8c971af1efab6838b57a912
ReyGraph <85826303+ReyGraph@users.noreply.github.com>
2023-07-29 16:52:19 +0530

    Visual Box Re-Weio

---
+++ script.js
@@ -0,0 +1,101 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+const navLinksDuplicate = navLinks.cloneNode(true); // Duplicate the nav-links
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+
+  // GSAP animation for burger menu lines
+  gsap.to('.line:nth-child(1)', {
+    y: navLinks.classList.contains('nav-active') ? 6 : 0,
+    rotate: navLinks.classList.contains('nav-active') ? 45 : 0,
+  });
+
+  gsap.to('.line:nth-child(2)', {
+    opacity: navLinks.classList.contains('nav-active') ? 0 : 1,
+  });
+
+  gsap.to('.line:nth-child(3)', {
+    y: navLinks.classList.contains('nav-active') ? -6 : 0,
+    rotate: navLinks.classList.contains('nav-active') ? -45 : 0,
+  });
+
+  // Toggle the duplicated nav-links with the burger menu activation
+  if (navLinks.classList.contains('nav-active')) {
+    navLinksDuplicate.classList.add('duplicate');
+    navLinksDuplicate.classList.remove('nav-active');
+    document.body.appendChild(navLinksDuplicate);
+  } else {
+    navLinksDuplicate.remove();
+  }
+});
+
+// Close the burger menu when a menu item is clicked
+const navItems = document.querySelectorAll('.nav-links li a, .nav-links.duplicate li a');
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navLinks.classList.remove('nav-active');
+    navLinksDuplicate.remove();
+    gsap.to('.line', { opacity: 1, y: 0, rotate: 0 });
+  });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+      link.addEventListener("click", scrollToSection);
+    });
+  });
+  
+  function scrollToSection(event) {
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const yOffset = -60; // Adjust scrolling position to account for the fixed navigation bar
+    const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: "smooth" });
+  }
+  
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const sections = document.querySelectorAll(".section");
+  
+  // Loop through each section to apply animations
+  sections.forEach((section, index) => {
+    // Animate the text elements in each section
+    const textElements = section.querySelectorAll("h2, p");
+    gsap.from(textElements, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      scrollTrigger: {
+        trigger: section,
+        start: "top 90%", // Start animation when the element is 90% inside the viewport
+        end: "bottom 60%", // End animation when the element is 60% inside the viewport
+        toggleActions: "play none none reverse", // Play the animation on entering the trigger area and reverse on exiting
+      },
+    });
+  
+    // Animate the logo text in the about section
+    if (section.id === "about") {
+      const logoText = section.querySelector("h1");
+      gsap.from(logoText, {
+        opacity: 0,
+        y: -50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: section,
+          start: "top 80%", // Start animation when the element is 80% inside the viewport
+          end: "bottom 60%", // End animation when the element is 60% inside the viewport
+          toggleActions: "play none none reverse", // Play the animation on entering the trigger area and reverse on exiting
+        },
+      });
+    }
+  });
+
+  
+  
